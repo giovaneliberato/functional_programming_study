25d09ff8b617d317654fd3bcf720bc75d28c26ce
Giovane <giovaneliberato@gmail.com>
2015-08-12 16:43:46 -0300

    exercicio finalizado

--- averageTemperature/average.py
+++ averageTemperature/average.py
@@ -1,6 +1,29 @@
 
 
+add_numbers = int.__add__
+
+def combine_arrays(array1, array2, final_array=None):
+    final_array = final_array or []
+
+    if len(array1) == 0 and len(array2) == 0:
+        return final_array
+
+    final_array.append([array1[0], array2[0]])
+    return combine_arrays(array1[1:], array2[1:], final_array)
+
+def average_for_array(array):
+    return average(total_for_array(array), len(array))
+
+def average(total, count):
+    return total / count
+
 def total_for_array(array, current_sum=0):
     if len(array) == 0:
-        return current_sum
-    return total_for_array(array[1:], current_sum) + array[0]
+        return 0
+    return reduce(add_numbers, array)
+
+def pluck(array, attr):
+    return map(get(attr), array)
+
+def get(attr):
+    return lambda item: item[attr]
--- averageTemperature/tests.py
+++ averageTemperature/tests.py
@@ -2,13 +2,70 @@ import unittest
 from average import *
 
 class AverageTests(unittest.TestCase):
-    def test_get_sum_of_list(self):
+    def test_get_sum_of_array(self):
         array = [1]
         self.assertEquals(total_for_array(array), 1)
         array = [1, 2, 3]
         self.assertEquals(total_for_array(array), 6)
         array = []
         self.assertEquals(total_for_array(array), 0)
+
+    def test_get_average_of_array(self):
+        array = [1, 2, 3]
+        self.assertEquals(average_for_array(array), 2)
+
+class DataStructureParsingTests(unittest.TestCase):
+    def setUp(self):
+        super(unittest.TestCase, self)
+        self.data = [
+          {
+            "name": "Jamestown",
+            "population": 2047,
+            "temperatures": [-34, 67, 101, 87]
+          },
+          {
+            "name": "Awesome Town",
+            "population": 3568,
+            "temperatures": [-3, 4, 9, 12]
+          }
+        ]
+
+    def test_get_temperatures(self):
+        expected = [[-34, 67, 101, 87], [-3, 4, 9, 12]]
+        self.assertListEqual(pluck(self.data, "temperatures"), expected)
+
+    def test_get_population(self):
+        expected = [2047, 3568]
+        self.assertListEqual(pluck(self.data, "population"), expected)
+
+
+class CombineArraysTests(unittest.TestCase):
+    def test_combine_two_arrays(self):
+        array1 = [2047, 3568]
+        array2 = [55, 5]
+        expected = [[2047, 55], [3568, 5]]
+        self.assertListEqual(combine_arrays(array1, array2), expected)
+
+class TestEverythingTogheter(unittest.TestCase):
+    def setUp(self):
+        super(unittest.TestCase, self)
+        self.data = [
+          {
+            "name": "Jamestown",
+            "population": 2047,
+            "temperatures": [-34, 67, 101, 87]
+          },
+          {
+            "name": "Awesome Town",
+            "population": 3568,
+            "temperatures": [-3, 4, 9, 12]
+          }
+        ]
+
+    def test_do_it(self):
+        expected = [[55, 2047], [5, 3568]]
+        processed = combine_arrays(map(average_for_array, pluck(self.data, "temperatures")), pluck(self.data, "population"))
+        self.assertListEqual(processed, expected)
 
 
 if __name__ == "__main__":
